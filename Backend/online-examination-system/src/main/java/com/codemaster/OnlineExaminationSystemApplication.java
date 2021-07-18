03376c15e6a0b08574d7fb48415cd541f15a9554
Kunal Bhangale <bhangalekunal2631996@gmail.com>
2021-07-18 13:21:30 +0530

    added breadcrum

--- Backend/online-examination-system/src/main/java/com/codemaster/OnlineExaminationSystemApplication.java
+++ Backend/online-examination-system/src/main/java/com/codemaster/OnlineExaminationSystemApplication.java
@@ -1,6 +1,7 @@
 package com.codemaster;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,12 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 public class OnlineExaminationSystemApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExaminationSystemApplication.class, args);
@@ -26,6 +30,7 @@ public class OnlineExaminationSystemApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin", "Content-Type",
 				"Accept","Jwt-Token","Authorization","Origin, Accept","X-Requested-With",
 				"Access-Control-Request-Method","Access-Control-Request-Headers"));
--- Frontend/OnlineExaminationSystem/src/app/pages/pages.component.ts
+++ Frontend/OnlineExaminationSystem/src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { MENU_ITEMS } from './pages-menu';
 
 @Component({
@@ -9,6 +10,8 @@ import { MENU_ITEMS } from './pages-menu';
 export class PagesComponent implements OnInit {
   menu = MENU_ITEMS;
   isOpened: boolean = false;
+  private titleSubject = new BehaviorSubject<any>({headingTitle:'demoheading',childTitle:'demochild'});
+  public titleAction$ = this.titleSubject.asObservable();
   constructor() {
    
    }
@@ -18,6 +21,12 @@ export class PagesComponent implements OnInit {
 
   toggleSidebar() {
     this.isOpened = !this.isOpened;
+  }
+
+  public changeTitle(heading: string, child: string): void
+  {
+    const titleObj = {headingTitle: heading,childTitle: child};
+      this.titleSubject.next(titleObj);
   }
 
 
